8b0ca935d626db17f28ee19b650533b1cc53e44c
Amit <amiitbhaik07@gmail.com>
2017-08-03 17:43:34 +0530

    Sixth commit

--- src/test/java/com/test/NGMDM_Tabs/_05_DealQualification.java
+++ src/test/java/com/test/NGMDM_Tabs/_05_DealQualification.java
@@ -1,5 +1,6 @@
 package com.test.NGMDM_Tabs;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -10,116 +11,37 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.test.BasicUtils.LaunchBrowsers;
+import com.test.BasicUtils.Modules;
+
 public class _05_DealQualification 
 {
-	@BeforeSuite
-	public void beforeSuite() throws Exception
-	{
-		System.out.println("Before Suite");
-	}
-	
-	@BeforeClass
-	public void beforeClass() throws Exception
-	{
-		System.out.println("Before Class");
-	}
-	
-	@BeforeTest
-	public void beforeTest() throws Exception
-	{
-		System.out.println("Before Test");
-	}
-	
-	@BeforeMethod
-	public void beforeMethod() throws Exception
-	{
-		System.out.println("Before Method");
-	}
+	static LaunchBrowsers browsers;
+	static String browserName = "firefox";
 	
 	@Test
-	public void test1() throws Exception
+	public void qfId1() throws Exception
 	{
-		System.out.println("Test1");
+		System.out.println("First Change");
+		System.out.println("Second Change");
+		System.out.println("_______________qfId1");
+		WebDriver driver = browsers.launchBrowser(browserName);
+		Modules m = new Modules(driver);
+		m.sfdcCreateOpportunityWithThaymore("QF_Form_ID_1");
+		m.sfdcFillCreateQuotePage(false);
+		m.quotingSetPreferences();
+		m.addDefaultProduct();
+		m.saveAndContinueFromItemsTab();
+		m.addSpecificNSDiscount(20, 0, 0);
+		m.saveAndContinueFromDiscountsTab();
+		m.selectReasonDropdownIfAppears_RNS_page();
+		m.saveAndContinueFromRnSTab();
+		m.continueToQualForm();
+		String dealID = m.getDealIDFromSFDCQualFormUI();
+		m.sfdcFillQualForm(1);
+		m.saveAndContinueFromQualForm();
+		m.submitDealForQualification(false);
+		m.sfdcAssert_DT2_SubmissionPage();		
+		System.out.println("Success : " + dealID);
 	}
-	
-	@Test
-	public void test2() throws Exception
-	{
-		System.out.println("Test2");
-	}
-	
-	@Test
-	public void test3() throws Exception
-	{
-		System.out.println("Test3");
-	}
-	
-	@Test
-	public void test4() throws Exception
-	{
-		System.out.println("Test4");
-	}
-	
-	@Test
-	public void test5() throws Exception
-	{
-		System.out.println("Test5");
-	}
-	
-	@Test
-	public void test6() throws Exception
-	{
-		System.out.println("Test6");
-	}
-	
-	@Test
-	public void test7() throws Exception
-	{
-		System.out.println("Test7");
-	}
-	
-	@Test
-	public void test8() throws Exception
-	{
-		System.out.println("Test8");
-	}
-	
-	@Test
-	public void test9() throws Exception
-	{
-		System.out.println("Test9");
-	}
-	
-	@Test
-	public void test10() throws Exception
-	{
-		System.out.println("Test10");
-	}
-	
-	@AfterMethod
-	public void afterMethod() throws Exception
-	{
-		System.out.println("After Method");
-	}
-	
-	@AfterTest
-	public void afterTest() throws Exception
-	{
-		System.out.println("After Test");
-	}
-	
-	@AfterClass
-	public void afterClass() throws Exception
-	{
-		System.out.println("After Class");
-	}
-	
-	@AfterSuite
-	public void afterSuite() throws Exception
-	{
-		System.out.println("After Suite");
-	}
-	
-
-
 }
